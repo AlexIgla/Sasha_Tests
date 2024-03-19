9954012b5c4aad5c91c37b2114eb848913e0dfdd
Александр <sanchez.ig.94@mai.ru>
2024-03-19 12:27:09 +0400

    #не сам

---
+++ 18_692_acmp.py
@@ -0,0 +1,6 @@
+#Бинарные числа
+n = int(input())
+if n==1 or n==2 or n==4 or n==8 or n==16 or n==32 or n==64 or n==128 or n==256 or n==512 or n==1024 or n==2048 or n==4096 or n==8192:
+    print('YES')
+else:
+    print('NO')
