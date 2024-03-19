4924ec401d1bf6f176dac890f3fb151955e2d4e4
Александр <sanchez.ig.94@mai.ru>
2024-03-19 15:45:28 +0400

    #не сам

--- 19_106_acmp.py
+++ 19_106_acmp.py
@@ -1,4 +1,11 @@
-#Монетки
-x = int(input())
-y = list(map(int, input().split()))
-print(y.count(0))
+# Монетки
+a = int(input())
+i, n, m = 0, 0, 0
+while i < a:
+    b = int(input())
+    i += 1
+    if b == 0:
+        n += 1
+    else:
+        m += 1
+print(min(n, m))
